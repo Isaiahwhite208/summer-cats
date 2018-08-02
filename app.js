27df316f0852443e00f150f12209a11cae8ad4ae
Isaiah White <isaiahwhite208@gmail.com>
2018-08-02 10:28:40 -0600

    js content

--- app.js
+++ app.js
@@ -4,7 +4,29 @@ const mrSnibbly = {
   'classy',
   'sassy'
  ],
+ moodImg: ['assets/cat.jpg', 'assets/madcat.jpg'],
  tolerance: 7,
  pets: 0,
  moodIndex: 0
 }
+
+const catImg = document.getElementById('cat-Img')
+const catName = document.getElementById('name')
+const mood = document.getElementById('mood')
+const pets = document.getElementById('pets')
+
+function draw() {
+ catImg.setAttribute('src', mrSnibbly.moodImg[mrSnibbly.moodIndex])
+ catName.innerText = mrSnibbly.name
+ mood.innerText = mrSnibbly.moods[mrSnibbly.moodIndex]
+ pets.innerText = mrSnibbly.pets.toString()
+}
+
+function pet() {
+
+}
+
+function reset() {
+
+}
+draw()
